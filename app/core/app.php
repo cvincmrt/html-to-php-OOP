3a0a946f4324e4556514a7ff5233e81cbeb84510
Martin Cvinček <cvincekm@spsknm.sk>
2025-11-19 10:02:32 +0100

    pridal som do funkcie urlSplit() isset

--- app/core/app.php
+++ app/core/app.php
@@ -44,6 +44,7 @@ class App {
      * function create array 
      */
     private function urlSplit(){
-        return explode("/", filter_var(trim($_GET["url"],"/"), FILTER_SANITIZE_URL));
+        $url = isset($_GET["url"]) ? $_GET["url"] : "home";
+        return explode("/", filter_var(trim($url,"/"), FILTER_SANITIZE_URL));
     }
 }
